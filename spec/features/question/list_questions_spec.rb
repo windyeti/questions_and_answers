10bde4723caa70a659e9fe231c7330fd29b46e6f
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-09-16 17:28:08 +0300

    add test owner? in user model

--- spec/features/question/list_questions_spec.rb
+++ spec/features/question/list_questions_spec.rb
@@ -5,11 +5,11 @@ feature 'Guest look at list of questions', %q{
     to given an answer
 } do
   given(:user) { create(:user) }
-  given!(:question) { create(:question, title: 'My title text', body: 'My body text text') }
+  given!(:questions) { create_list(:question, 4, title: 'My title text', body: 'My body text text 456') }
 
   scenario 'look list of questions' do
     visit questions_path
     expect(page).to have_content('My title text')
-    expect(page).to have_content('My body text text')
+    expect(page).to have_content('My body text text 456')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password) }
   it { should have_many :questions }
   it { should have_many :answers }
+
+  describe 'User' do
+    let(:user) { create(:user) }
+
+    context 'is owner resource' do
+      let(:question) { create(:question, user: user) }
+
+      it do
+        expect(user).to be_owner(question)
+      end
+    end
+
+    context 'is not owner resource' do
+      let(:question) { create(:question) }
+
+      it do
+        expect(user).to_not be_owner(question)
+      end
+    end
+  end
 end
