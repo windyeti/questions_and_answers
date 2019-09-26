f220121f3602197d4c4c2b0865fd2c952c6a74bf
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-09-26 15:37:08 +0300

    add test checking the place best answer

--- spec/features/answer/best_spec.rb
+++ spec/features/answer/best_spec.rb
@@ -33,10 +33,43 @@ feature 'Only author can select best of question' do
   context 'Guest user not author ' do
     scenario 'can not select best answer' do
       visit question_path(question)
-      save_and_open_page
 
       expect(page).to have_css('a.hide_link_best')
     end
   end
+
+    context 'Only one answer should be best', js: true do
+      background { sign_in(user) }
+      given!(:answer_start_best) { create(:answer, question: question, body: 'The best answer', best: true) }
+
+      scenario 'Set other answer as best' do
+        visit question_path(question)
+
+        click_on 'Best'
+
+        sleep(1)
+
+        answer.reload
+        answer_start_best.reload
+
+        sleep(3)
+
+        expect(answer.best).to be true
+        expect(answer_start_best.best).to be false
+      end
+    end
+
+    context 'Must be the first best answer on the list', js: true do
+      background { sign_in(user) }
+      given!(:answer_start_best) { create(:answer, question: question, body: 'The best answer', best: true) }
+
+      scenario 'checking the number of best answer' do
+        visit question_path(question)
+
+        within '.answer:first-child' do
+          expect(page).to have_content'The best answer'
+        end
+      end
+    end
 
 end
