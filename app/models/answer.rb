3106dc5abcea73389a09e51070f4b56dbaa477f1
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-09-26 17:20:05 +0300

    add helper j and validates :best

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+  validates_inclusion_of :best, in: [true, false]
 
   default_scope { order(best: :desc) }
 
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:body) }
+  it { should validate_inclusion_of(:best).in_array([true, false]) }
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
