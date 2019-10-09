549b72ed6bfff682892a2e53663b8dd5beec7e2c
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-10-09 11:18:43 +0300

    aded two feature test for Attachment

--- spec/features/attachment/attachment_spec.rb
+++ spec/features/attachment/attachment_spec.rb
@@ -22,26 +22,26 @@ feature 'Attachment files' do
     end
   end
 
-  # context "Authenticated user not author" do
-  #   given(:other_user) { create(:user) }
-  #   background { sign_in(other_user) }
-  #
-  #   scenario 'can not delete attached files to question' do
-  #     visit question_path(question)
-  #
-  #     within '.question' do
-  #       expect(page).to_not have_selector(:link_or_button, 'Delete')
-  #     end
-  #   end
-  # end
-  #
-  # context "Guest" do
-  #   scenario 'can not delete attached files to question' do
-  #     visit question_path(question)
-  #
-  #     within '.question' do
-  #       expect(page).to_not have_selector(:link_or_button, 'Delete')
-  #     end
-  #   end
-  # end
+  context "Authenticated user not author" do
+    given(:other_user) { create(:user) }
+    background { sign_in(other_user) }
+
+    scenario 'can not delete attached files to question' do
+      visit question_path(question)
+
+      within '.question' do
+        expect(page).to_not have_selector(:link_or_button, 'Delete')
+      end
+    end
+  end
+
+  context "Guest" do
+    scenario 'can not delete attached files to question' do
+      visit question_path(question)
+
+      within '.question' do
+        expect(page).to_not have_selector(:link_or_button, 'Delete')
+      end
+    end
+  end
 end
