84ac2120993eb78f0b8d94d36fd03a2eab419182
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-10-18 14:35:36 +0300

    add test of model answer User get reward

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -10,7 +10,20 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of(:body) }
 
-  it 'have one attached file' do
+  it 'have many attached file' do
     expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
+
+  context 'Create question with reward and answer' do
+      let(:user_question) { create(:user) }
+      let(:user_answer) { create(:user) }
+      let(:question) { create(:question, :with_reward, user: user_question) }
+      let(:answer) { create(:answer, question: question, user: user_answer) }
+
+    it 'user get reward' do
+      answer.set_best
+
+      expect(user_answer.rewards).to match_array [question.reward]
+    end
   end
 end
