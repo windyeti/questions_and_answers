613fb6af3ded21ba42d5d64ba3a8f80f0a9cc850
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-10-25 12:44:16 +0300

    add add concern WithVote

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -1,28 +1,50 @@
 module Voted
   extend ActiveSupport::Concern
+  include WithVote
 
   included do
-    before_action :vote_init, only: [:vote_up, :vote_down]
+    before_action :set_voteable, only: [:vote_up, :vote_down, :vote_reset]
   end
 
   def vote_up
-    @vote.voteup = true
-    vote_response
+    respond_to do |format|
+      if current_user && !current_user&.owner?(@voteable) && @voteable.can_vote?(current_user)
+        @vote = Vote.new(voteable: @voteable, user: current_user)
+        @vote.voteup = true
+        if @vote.save
+          format.json { render_json_ok_response }
+        else
+          format.json { render_json_with_errors }
+        end
+      else
+        format.json { render_json_with_denied }
+      end
+    end
   end
 
   def vote_down
-    vote_response
+    respond_to do |format|
+      if current_user && !current_user&.owner?(@voteable) && @voteable.can_vote?(current_user)
+        @vote = Vote.new(voteable: @voteable, user: current_user)
+        if @vote.save
+          format.json { render json: { vote: "You are vote", value: @voteable.balance_votes } }
+        else
+          format.json { render json: @vote.errors.full_messages, status: :unprocessable_entity }
+        end
+      else
+        format.json { render json: { vote: "You are not rights for this act" }, status: 401 }
+      end
+    end
   end
 
   def vote_reset
-    set_voteable
     if current_user && !current_user&.owner?(@voteable) && !@voteable.can_vote?(current_user)
       @vote = @voteable.user_vote(current_user)
 
       respond_to do |format|
         if @vote.destroy
           format.json do
-            render json: { vote: "You vote destroy", value: @voteable.balance_votes  }.to_json
+            render json: { vote: "You vote reset", value: @voteable.balance_votes  }.to_json
           end
         else
           format.json do
@@ -37,30 +59,30 @@ module Voted
 
   private
 
-  def vote_response
-    respond_to do |format|
-      if @vote.save
-        format.json do
-          render json: { vote: "You voted", value: @voteable.balance_votes }.to_json
-        end
-      else
-        format.json do
-          render json: @vote.errors.full_messages, status: :unprocessable_entity
-        end
-      end
-    end
-  end
+  # def vote_response
+  #   respond_to do |format|
+  #     if @vote.save
+  #       format.json do
+  #         render json: { vote: "You voted", value: @voteable.balance_votes }.to_json
+  #       end
+  #     else
+  #       format.json do
+  #         render json: @vote.errors.full_messages, status: :unprocessable_entity
+  #       end
+  #     end
+  #   end
+  # end
 
-  def vote_init
-    set_voteable
-    if current_user && !current_user&.owner?(@voteable) && @voteable.can_vote?(current_user)
-      @vote = Vote.new(voteable: @voteable, user: current_user)
-    else
-      respond_to do |format|
-        format.json { render json: { vote: "You do not have rights to act" }.to_json, status: 401}
-      end
-    end
-  end
+  # def vote_init
+  #   set_voteable
+  #   if current_user && !current_user&.owner?(@voteable) && @voteable.can_vote?(current_user)
+  #     @vote = Vote.new(voteable: @voteable, user: current_user)
+  #   else
+  #     respond_to do |format|
+  #       format.json { render json: { vote: "You do not have rights to act" }.to_json, status: 401}
+  #     end
+  #   end
+  # end
 
   def get_klass
     controller_name.classify.constantize
---
+++ app/controllers/concerns/with_vote.rb
@@ -0,0 +1,24 @@
+module WithVote
+  def render_json_ok_response
+    # render json: { vote: "You are vote", value: @voteable.balance_votes }
+    render json: Hash[:vote, "You are vote", :value, set_resource.balance_votes]
+  end
+
+  def render_json_with_errors
+    render json: @vote.errors.full_messages, status: :unprocessable_entity
+  end
+
+  def render_json_with_denied
+    render json: { vote: "You are not rights for this act" }, status: 401
+  end
+
+  private
+
+  def get_klass
+    controller_name.classify.constantize
+  end
+
+  def set_resource
+    get_klass.find(params[:id])
+  end
+end
