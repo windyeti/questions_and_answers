89c0f655216daf42e8bf9355bfd70ae4b812e9de
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-01-09 15:12:54 +0300

    fix postgresql

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,6 @@ class SearchController < ApplicationController
   skip_authorization_check
   def index
     @results = ThinkingSphinx.search(params_search)
-    p '::::::::::::::::::::::::::::'
-    p Question.all
-    p '::::::::::::::::::::::::::::'
-    p '>>>>>>>>>>>>>>>>>>>>>>>'
-    p @results
-    p '>>>>>>>>>>>>>>>>>>>>>>>'
-    render :index
-    # if params_search.present?
-    # end
   end
 
   private
--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -1,20 +1,13 @@
 require 'sphinx_helper'
 
-feature 'User can search for answer' do
+feature 'User can search' do
   given!(:question) { create(:question, body: 'My question body') }
+  background { visit questions_path }
 
-  background do
-    visit questions_path
-  end
-  scenario 'User searches for the answer', js: true, sphinx: true do
+  scenario 'User searches for the question', js: true, sphinx: true do
     ThinkingSphinx::Test.run do
       fill_in 'Search', with: 'My question body'
       click_on 'Search'
-      p '++++++++++++++++++++++++'
-      p Question.all
-      p '++++++++++++++++++++++++'
-save_and_open_page
-      sleep 200
       expect(page).to have_content 'My question body'
     end
   end
