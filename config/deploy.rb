6b05b1d4145155bb588e79f5cdca4b624ffee9be
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2020-01-29 17:18:52 +0300

    fix config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, "/home/deployer/qna"
 set :deploy_user, "deployer"
 
 # Postgresql
-set :pg_password, Rails.application.credentials.postgres[:pg_password]
+set :pg_password, "#{Rails.application.credentials.postgres[:pg_password]}"
 set :pg_ask_for_password, true
 
 # Default value for :format is :airbrussh.
